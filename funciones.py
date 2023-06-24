33fddbfc45eae8e12fba74acc146c174f356ef66
Yarod Aliaga <yarod.lanas@gmail.com>
2023-06-24 02:49:38 -0400

    Se avanza con la función para grabar vehiculos, tengo que corregir lo siguiente: Multas, el while esta mal puesto. ¿Por qué no quedan varios diccionarios en la lista? Y quizá limitar los tipos de auto

---
+++ funciones.py
@@ -0,0 +1,121 @@
+def grabar():
+    while True:
+        def verificar_patente(patente):
+            return 6 >= len(patente) #Queria intentar verificar que fueran 4 letras y 2 números, encontre una forma con expresiónes regulares pero no entendí demasiado.
+
+        def verificar_marca(marca):
+            return 5 <= len(marca) <= 12 #Hice estas tres funciones para entenderme un poco más, creo que dividiré el codigo en una función por archivo
+
+        def verificar_precio(precio): #Vericia precio
+            if precio >= 500000:
+                return True
+            
+        autos = [] #Guarda los autos ingresados, no sé bien como usarlo como un array de numpy. No funciona, entiendo que los diccionarios son unicos ¿Pero no puedo meter diferentes en un array o lista?
+  
+        auto = { #Diccionario para guardar los autos
+            'Tipo' : input('Ingrese el tipo de auto: '),
+            'Patente': '',
+            'Marca': '',
+            'Precio': '',
+            'Multas': {
+                'Fecha' : [],
+                'Monto' : [],
+            },
+            'Fecha registro': input('Ingrese la fecha de registro: '),
+            'Dueño' : {
+                'Nombre' : input('Ingrese el nombre del dueño del auto: '),
+                'Apellido' : input('Ingrese el apellido paterno del dueño del auto: '),
+                'RUN' : ''
+            }
+            }
+        
+        while True:
+            run = input('Ingresar rut del dueño (Sin puntos ni guión): ')
+            if len(run) >= 8: #Verifica si el rut es igual o mayor a 8 digitos, si es así normaliza el guión (Sacado del ejercicio de array)
+                run = run[:-1] + "-" + run[-1]
+                auto['Dueño']['RUN'] = run
+                break
+            else:
+                print('Datos invalidos')
+                continue
+        
+        while True: #ingreso de patente
+            patente = input('Ingrese la patente del vehiculo: ')
+            if verificar_patente(patente) == True:
+                auto['Patente'] = patente
+                break
+            else:
+                print('Datos no validos, reintentar')
+                continue
+
+        while True: #Ingreso de marca
+            marca = input('Ingrese la marca del vehiculo: ')
+            if verificar_marca(marca) == True:
+                auto['Marca'] = marca
+                break
+            else:
+                print('Datos no validos, reintentar')
+                continue
+        
+        while True: #Ingreso de precio
+            precio = input('Ingrese el precio del vehiculo: ')
+            try:
+                precio = int(precio)
+            except:
+                print('Datos no validos, reintentar')            
+                continue
+
+            if verificar_precio(precio) == True:
+                auto['Precio'] = precio
+                break
+            else:
+                print('Datos no validos, reintentar')
+                continue
+
+        while True: #Ingreso de multas
+
+            verificacion = input('¿El vehiculo posee multas?(S/N): ') #Verificamos que posea multas
+            verificacion = verificacion.upper()
+            if verificacion == 'S': #Si tiene, ingresamos la fecha y validamos que el valor esté en formato integeR
+                while True:
+                    fecha_multa = input('Ingrese la fecha de la multa en formato (DD/MM/AA): ')
+                    monto_multa = input('Ingrese el valor de la multa: ')
+                    try:
+                        monto_multa = int(monto_multa)
+                    except:
+                        print('Los datos deben ser numericos')
+                        continue
+
+                    auto['Multas']['Fecha'].append(fecha_multa) #Agregamos a la lista los datos de las fechas ingresadas
+                    auto['Multas']['Monto'].append(monto_multa) #Agregamos a la lista los datos de los valores ingresados
+
+                    seguir = input('¿Desea seguir ingresando multas?(S/N): ') #Verificación para saber si el usuario quiere seguir agregando multas
+                    seguir = seguir.upper()
+                    break
+
+                if seguir == 'S': #Seguir en el ingreso de multas en caso de generar más
+                    continue
+                elif seguir == 'N': #Salir del ingreso de multas en caso de no querer seguir
+                    break
+                else: 
+                    print('Datos no validos, reintentar')
+                    continue
+
+            elif verificacion == 'N': #Salir del ingreso de multas en caso de no tener
+                break
+            else:
+                print('Datos no validos, reintentar')
+                continue
+
+        autos.append(auto)        
+        ingreso = input('¿Desea seguir ingresando autos? (S/N)')
+        ingreso = ingreso.upper()
+        if ingreso == 'S':
+            continue
+        elif ingreso == 'N':
+            break
+        
+
+    print(autos)
+
+grabar()
